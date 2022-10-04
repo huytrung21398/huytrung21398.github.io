d5fc3db9d07e206083dc7a0ad1d9df1e97c12675
HuyTrung <huytrung213@gmail.com>
2022-10-05 00:51:15 +0700

    commit-05-10-2022

--- giao-dien/javascript/script.js
+++ giao-dien/javascript/script.js
@@ -9,10 +9,50 @@
 //         toP.classList.remove("active")
 //     }
 // })
-// Menu-slider-category------------
+
+
+// MENU SLIDER CATEGORY------------
 const itemSliderBar = document.querySelectorAll(".category-left-li")
 itemSliderBar.forEach(function(menu,index){
     menu.addEventListener("click",function(){
         menu.classList.toggle("block")
+    })
+})
+// PRODUCT---------------------
+const baoquan = document.querySelector(".baoquan")
+const chitiet = document.querySelector(".chitiet")
+const kichthuoc = document.querySelector(".kichthuoc")
+if(baoquan){
+    baoquan.addEventListener("click", function(){
+        document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "none"
+        document.querySelector(".product-content-right-bottom-content-baoquan").style.display = "block"
+        document.querySelector(".product-content-right-bottom-content-kichthuoc").style.display = "none"
+    })
+}
+if(chitiet){
+    chitiet.addEventListener("click", function(){
+        document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "block"
+        document.querySelector(".product-content-right-bottom-content-baoquan").style.display = "none"
+        document.querySelector(".product-content-right-bottom-content-kichthuoc").style.display = "none"
+    })
+}
+if(kichthuoc){
+    kichthuoc.addEventListener("click", function(){
+        document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "none"
+        document.querySelector(".product-content-right-bottom-content-baoquan").style.display = "none"
+        document.querySelector(".product-content-right-bottom-content-kichthuoc").style.display = "block"
+    })
+}
+const button = document.querySelector(".product-content-right-bottom-top")
+    if(button){
+        button.addEventListener("click", function(){
+            document.querySelector(".product-content-right-bottom-content-big").classList.toggle("activeB")
+        })
+    }
+const bigImg = document.querySelector(".product-content-left-big-img img")
+const smallImg = document.querySelectorAll(".product-content-left-small-img img")
+smallImg.forEach(function(imgItem,X){
+    imgItem.addEventListener("click", function(){
+        bigImg.src = imgItem.src
     })
 })
